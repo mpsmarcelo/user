d8aa2109018da1301dc7de8b1226a5783f543d24
Marcelo Pereira <marcellopsnet@gmail.com>
2023-09-20 21:57:54 -0300

    Implementação de objeto dto retorno dos metodos.

--- src/main/java/com/marcelo/service/UserService.java
+++ src/main/java/com/marcelo/service/UserService.java
@@ -1,38 +1,53 @@
 package com.marcelo.service;
 
 
-import com.marcelo.entity.User;
+import com.marcelo.dto.UserDTO;
+import com.marcelo.entity.Usuario;
 import com.marcelo.repository.UserRepository;
+import org.hibernate.dialect.pagination.AbstractLimitHandler;
+import org.hibernate.sql.results.internal.StandardRowReader;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
 
     private UserRepository repository;
 
+    @Autowired
+
+
     public UserService(UserRepository repository) {
         this.repository = repository;
     }
 
 
-    List<User> list(){
-        return repository.findAll(Sort.by("nome").ascending());
+    public List<UserDTO> list(){
+        ModelMapper modelMapper = new ModelMapper();
+        return repository.findAll(Sort.by("nome").ascending())
+                .stream()
+                .map((user) -> modelMapper.map(user, UserDTO.class))
+                .collect(Collectors.toList());
     }
 
-    List<User> create(User user){
+    public  List<UserDTO> create(Usuario user){
         repository.save(user);
         return list();
     }
 
-    List<User> delete(Long id){
+    public  List<UserDTO> delete(Long id){
         repository.deleteById(id);
         return list();
     }
 
-    List<User> update(User user){
+    public  List<UserDTO> update(Usuario user){
         repository.save(user);
         return list();
     }
