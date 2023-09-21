2791752b2eeb5e2d7c991193197fe93ca7896569
Marcelo Pereira <marcellopsnet@gmail.com>
2023-09-20 21:55:11 -0300

    Alteração nome objeto.

--- src/main/java/com/marcelo/repository/UserRepository.java
+++ src/main/java/com/marcelo/repository/UserRepository.java
@@ -1,10 +1,10 @@
 package com.marcelo.repository;
 
-import com.marcelo.entity.User;
+import com.marcelo.entity.Usuario;
+import org.hibernate.tool.schema.UniqueConstraintSchemaUpdateStrategy;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
-
+public interface UserRepository extends JpaRepository<Usuario,Long> {
 }
